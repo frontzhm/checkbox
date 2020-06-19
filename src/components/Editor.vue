de9e2b613ca748e1bdbade22f3fddc3629e74085
zhm <frontzhm@163.com>
2020-06-19 11:21:38 +0800

    v3-local-pic

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -40,6 +40,8 @@ export default {
       this.editor = editor;
       // 将富文本的html的内容变化时赋值同步到editorContent，这里的change事件将值赋值给editorContent
       this._syncContent();
+      // 设置上传本地图片
+      this._setUploadLocalImg();
       // 创建编辑器
       editor.create();
     },
@@ -52,6 +54,39 @@ export default {
     },
     _setInitContent(content) {
       this.editor.txt.html(content);
+    },
+    _setUploadLocalImg() {
+      this.editor.customConfig.customUploadImg = async (files, insert) => {
+        // files 是 input 中选中的文件列表，遍历上传
+        for (let i = 0; i < files.length; i++) {
+          let file = files[i];
+          // 上传服务器拿到图片链接,这里_uploadSingleFile就是上传图片接口，
+          let imgUrl = await this._uploadSingleFile(file);
+          // 获取图片 url 后，插入到编辑器
+          insert(imgUrl);
+        }
+      };
+    },
+    // 因项目而异
+    async _uploadSingleFile(file) {
+      let options = {
+        url: "/xx/yy",
+        method: "POST",
+        data: { sign: "xxx", file },
+        // 转化成formData形式
+        transformRequest: [
+          function(data) {
+            let formData = new FormData();
+            for (const key in data) {
+              const value = data[key];
+              formData.append(key, value);
+            }
+            return formData;
+          }
+        ]
+      };
+      let url = (await "axios"(options)).data.url;
+      return url;
     }
   }
 };
